18b03fe6e7afbb5de0920523d38b7f7f51414e82
Vinicius Mesel <viniciuscarqueijo@gmail.com>
2018-03-09 18:54:11 -0300

    Add new visualizer for cytoscape and its generation code

---
+++ data/generate_cytoscape_modified_for_vis.py
@@ -0,0 +1,31 @@
+import json
+
+list_sergio = [
+    "integral to membrane",
+    "intrinsic to membrane",
+    "extracellular region",
+    "extracellular region part",
+    "membrane part",
+    "membrane",
+    "extracellular space",
+    "DNA integration",
+    "RNA-directed DNA polymerase activity",
+    "proteinaceous extracellular matrix",
+    "RNA-dependent DNA replication",
+    "extracellular matrix",
+    "dynein complex",
+    "monovalent inorganic cation transmembrane transporter",
+]
+
+with open("/home/vmesel/GitHub/site_xto/static/cytoscape/CoexpNetwork-cor0p8-DEedgeDE-lincSMP-181mainInt_v2.cyjs", "r") as js:
+    a = json.load(js)
+
+    for item in a["elements"]["nodes"]:
+        if item["data"]["name"] in list_sergio:
+            item["data"]["pValue_GO_DE_2359SMPs_cor0p8_181lincRNAs"] = 1.000000
+            # print(item["data"])
+        else:
+            item["data"]["pValue_GO_DE_2359SMPs_cor0p8_181lincRNAs"] = 0.000000
+
+with open("/home/vmesel/GitHub/site_xto/static/cytoscape/new_cyto2.cyjs", 'w') as b:
+    json.dump(a, b)
